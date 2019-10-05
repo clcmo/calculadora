2eeccc279181aca8a1482af6a888848083b4bfbb
Camila <camila.leite.oliveira@gmail.com>
2019-10-05 01:17:57 -0300

    Create Calc and Screen classes

--- Calc.java
+++ Calc.java
@@ -25,21 +25,22 @@ public class Calc {
  
     private String calcValue(List<int> listNum, List<Character> listOps) {
         String res = "";
-        int total = 0, j = 0, n1, n2;
+        int t = 0, j = 0, n1, n2;
+        char op;
         for (int i = 0; i < listNum.size()-1; i++) {
-            if (total == 0) {
+            if (t == 0) {
                 n1 = listNum.get(i).intValue();
                 n2 = listNum.get(i + 1).intValue();
-                char op = listOps.get(i).charValue();
-                total = operate(n1, op, n2);
-            } else if (total > 0) {
+                op = listOps.get(i).charValue();
+                t = operate(n1, op, n2);
+            } else if (t > 0) {
                 n2 = listNum.get(i).intValue();
-                char op = listOps.get(j).charValue();
-                total = operate(total, op, n2);
+                op = listOps.get(j).charValue();
+                t = operate(t, op, n2);
                 j++;
             }
         }
-        res = ""+total;
+        res = ""+t;
         return res;
     }
  
@@ -56,10 +57,9 @@ public class Calc {
     }
  
     private List<int> getNums(String input) {
- 
         List<int> listNum = new ArrayList<int>();
- 
         String numToString = "";
+
         for (int i = 0; i < input.length(); i++) {
             if(isop(input.charAt(i))) {
                 int num = int.valueOf(numToString);
