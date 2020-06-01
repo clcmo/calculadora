2f761c875f2b67c7158de02d172bcc10ece7a3b4
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-05-31 21:27:36 -0300

    Update Calculadora: Estilo atualizado

--- src/ExitListener.java
+++
@@ -1,9 +0,0 @@
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-class ExitListener extends WindowAdapter {
-
-    public void windowClosing(WindowEvent e) {
-        System.exit(0);
-    }
-}
